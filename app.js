91e193b2693fec64168baab43569e8ca0d790c78
Anushka Shukla <anushkashukla3003@gmail.com>
2026-04-22 22:41:42 +0530

    update app.js-006

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send("Hello from Kubernetes! This is the app.js file. and its my test msg 005....");
+  res.send("Hello from Kubernetes! This is the app.js file. and its my test msg 006....");
 });
 
 app.get('/health', (req, res) => {
