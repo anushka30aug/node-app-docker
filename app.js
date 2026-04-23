76ed89697f851a8a4edf91408aaa83bc6474c23c
Anushka Shukla <anushkashukla3003@gmail.com>
2026-04-23 10:17:54 +0530

    change app 008 and perform failure

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send("Hello from Kubernetes! This is the app.js file. and its my test msg 007....");
+  res.send("Hello from Kubernetes! This is the app.js file. and its my test msg 008....");
 });
 
 app.get('/health', (req, res) => {
-  res.sendStatus(200);
+  res.sendStatus(500);
 });
 
 app.listen(port, '0.0.0.0', () => {
