b5533df4e385c63bb638f4158e23fecf8d09b2ec
Anushka Shukla <anushkashukla3003@gmail.com>
2026-04-23 11:58:43 +0530

    change app 0011 fail delibrately

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send("Hello from Kubernetes! This is the app.js file. and its my test msg 0010....");
+  res.send("Hello from Kubernetes! This is the app.js file. and its my test msg 0011....");
 });
 
 app.get('/health', (req, res) => {
   res.sendStatus(200);
 });
 
-app.listen(80, '0.0.0.0', () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server running on port ${port}`);
+  throw new Error("This is a test error to check the error handling in Kubernetes.");
 });
