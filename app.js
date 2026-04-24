14bd7136c13d54171c03f753dfc726e9bbedfebb
Anushka Shukla <anushkashukla3003@gmail.com>
2026-04-24 15:38:29 +0530

    change app.js 0012

--- app.js
+++ app.js
@@ -3,14 +3,13 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send("Hello from Kubernetes! This is the app.js file. and its my test msg 0011....");
+  res.send("Hello from Kubernetes! This is the app.js file. and its my test msg 0012....");
 });
 
 app.get('/health', (req, res) => {
-  res.sendStatus(200);
+  res.sendStatus(500);
 });
 
 app.listen(port, '0.0.0.0', () => {
   console.log(`Server running on port ${port}`);
-  throw new Error("This is a test error to check the error handling in Kubernetes.");
 });
