1b9759435239f4926ffb7938f0e64a5425c29606
Anushka Shukla <anushkashukla3003@gmail.com>
2026-04-24 20:08:23 +0530

    change app.js 0016 intentionally fail

--- app.js
+++ app.js
@@ -3,12 +3,11 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.sendStatus(500);
-  res.send("Hello from Kubernetes! This is the app.js file. and its my test msg 0015....");
+  res.send("Hello from Kubernetes! This is the app.js file. and its my test msg 0016....");
 });
 
 app.get('/health', (req, res) => {
-  res.sendStatus(200);
+  res.sendStatus(500);
 });
 
 app.listen(port, '0.0.0.0', () => {
