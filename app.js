6881a24779bc09c2b3a12883b5d352b9d36f7739
Anushka Shukla <anushkashukla3003@gmail.com>
2026-04-27 11:29:27 +0530

    change app.js 0022

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const port = 3000;
 
 app.get('/', (req, res) => {
   // res.sendStatus(404);
-  res.send("Hello from Kubernetes! This is the app.js file. and its my test msg 0021....");
+  res.send("Hello from Kubernetes! This is the app.js file. and its my test msg 0022....");
 });
 
 app.get('/health', (req, res) => {
-  res.sendStatus(200);
+  res.sendStatus(404);
 });
 
 app.listen(port, '0.0.0.0', () => {
