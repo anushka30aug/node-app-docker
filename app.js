f6e08ec2a4797da39a02af47979565079e2f8881
Anushka Shukla <anushkashukla3003@gmail.com>
2026-05-01 16:02:31 +0530

    change msg 33

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(metricsMiddleware);
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send("Hello from Kubernetes!32");
+  res.send("Hello from Kubernetes!33");
 });
 
 app.get('/health', (req, res) => {
